f10bc4d52ee67b78a3e21baa9a5d8a9f5138c3a9
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2024-01-28 22:58:54 +0530

    Update authorization.js

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -4,54 +4,33 @@ const router = express.Router();
 const { google } = require("googleapis");
 const url = require("url");
 const querystring = require("querystring");
-
-router.post("/", function main(req, res) {
+router.post("/", async function main(req, res) {
   const parsedUrl = url.parse(req.url);
   const queryParameters = querystring.parse(parsedUrl.query);
-
-  function authorize() {
-    const CLIENT_ID = process.env.CLIENT_ID;
-    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+  async function authorize() {
+    const CLIENT_ID =process.env.CLIENT_ID      ;
+    const CLIENT_SECRET =process.env.CLIENT_SECRET;
     const REDIRECT_URI = process.env.REDIRECT_URI;
-
+    
     // Create an OAuth2 client with the client ID and client secret
     const oAuth2Client = new google.auth.OAuth2(
       CLIENT_ID,
       CLIENT_SECRET,
+
       REDIRECT_URI
     );
-
-    getAccessToken(oAuth2Client)
-      .then((token) => {
+    getAccessToken(oAuth2Client);
+    function getAccessToken(oAuth2Client) {
+      const code = queryParameters.code;
+      if (!code) console.log("Code is not present in request");
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error("Error retrieving access token"+err);
+        // Store the token for future use
         oAuth2Client.setCredentials(token);
         res.send(oAuth2Client);
-      })
-      .catch((err) => {
-        console.error("Error retrieving access token", err);
-        res.status(500).send("Error retrieving access token");
       });
-  }
-
-  function getAccessToken(oAuth2Client) {
-    const code = queryParameters.code;
-
-    if (!code) {
-      console.log("Code is not present in request");
-      return Promise.reject(new Error("Code is not present in request"));
     }
-
-    return new Promise((resolve, reject) => {
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(token);
-        }
-      });
-    });
   }
-
-  authorize();
+  authorize();  
 });
-
 module.exports = router;
